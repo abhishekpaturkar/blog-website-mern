8d7f2411b767dad25a3a32aae06fa4f4a7ab042d
abhishekpaturkar <godkillop03@gmail.com>
2024-03-11 15:40:51 +0530

    comment routes and image upload route almost done

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 // dotenv config
 dotenv.config();
@@ -12,21 +13,40 @@ import authRoute from "./routes/auth.js";
 import userRoute from "./routes/user.js";
 import postRoute from "./routes/posts.js";
 import commentRoute from "./routes/comment.js";
+import multer from "multer";
 
 // Constants
 const app = express();
 const PORT = process.env.PORT || 3001;
+const __dirname = path.resolve();
 
 // Middleaware
-app.use(cookieParser());
-app.use(cors());
+
 app.use(express.json());
+app.use("/images", express.static(path.join(__dirname, "/backend", "/images")));
+app.use(cors());
+app.use(cookieParser());
 
 //Routes
 app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/comments", commentRoute);
+
+// image upload
+const storage = multer.diskStorage({
+  destination: (req, file, fn) => {
+    fn(null, "images");
+  },
+  filename: (req, file, fn) => {
+    fn(null, req.body.img);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("Image has been uploaded successfully");
+});
 
 app.listen(PORT, () => {
   connectToDB();
--- backend/routes/comment.js
+++ backend/routes/comment.js
@@ -1,5 +1,56 @@
 import express from "express";
+import { verifyToken } from "../middlewares/verifyToken.js";
+import Comment from "../models/comment.js";
 
 const router = express.Router();
+
+// create
+router.post("/create", verifyToken, async (req, res) => {
+  try {
+    const newComment = new Comment(req.body);
+    const savedComment = await newComment.save();
+    res.status(200).json(savedComment);
+  } catch (error) {
+    console.log("Error in comment /create", error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Update
+router.put("/:id", verifyToken, async (req, res) => {
+  try {
+    const updatedComment = await Comment.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json(updatedComment);
+  } catch (error) {
+    console.log("Error in comment put /:id", error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// delete
+router.delete("/:id", verifyToken, async (req, res) => {
+  try {
+    await Comment.findByIdAndDelete(req.params.id);
+    res.status(200).json("Comment has been deleted");
+  } catch (error) {
+    console.log("Error in comment delete /:id", error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Get post comment
+router.get("/post/:postId", async (req, res) => {
+  try {
+    const comments = await Comment.find({ postId: req.params.postId });
+    res.status(200).json(comments);
+  } catch (error) {
+    console.log("Error in comment by post /post/:postId", error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
 
 export default router;
